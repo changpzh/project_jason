ececcb6de456a4dcd28ed258f50ff19a39d4bd5f
Changping <changping.zhou@foxmail.com>
2017-05-17 15:37:30 +0800

    add markdown_syntax and python_robotframework

---
+++ 01_Python_project/01_python_robotframework/0102_automation_of_coop/01_run_atest.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+def get_libpath():
+	return os.path.join(os.path.dirname(__file__), 'libraries')
+
+def get_suitepath():
+	return os.path.join(os.path.dirname(__file__), 'falcon')
+
+def main():
+
+if __name__ == '__main__':
+	main()
